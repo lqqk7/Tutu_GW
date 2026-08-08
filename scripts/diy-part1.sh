c75ee4cccb54cc8c6612223f9a5ab9d009971280
litutu <tutu@tutuben.local>
2026-08-09 06:26:31 +0800

    feat(firmware): bake FTTR gateway defaults and Chinese translations

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -11,12 +11,15 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
 
 # ddns-go LuCI 界面 (v6.9.4，v6.16.x 要求 Go 1.25 与 OpenWrt 24.10 不兼容)
 git clone --depth=1 -b v6.9.4 https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
+cp "$GITHUB_WORKSPACE/patches/luci-i18n/ddns-go.zh_Hans.po" package/luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans/ddns-go.po
 
 # Advanced Plus
 git clone --depth=1 https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
+cp "$GITHUB_WORKSPACE/patches/luci-i18n/advancedplus.zh_Hans.po" package/luci-app-advancedplus/po/zh_Hans/advancedplus.po
 
 # Autoupdate (支持 GitHub API 自动检测版本并更新)
 git clone --depth=1 https://github.com/xztxy/luci-app-autoupdate.git package/luci-app-autoupdate
+cp "$GITHUB_WORKSPACE/patches/luci-i18n/autoupdate.zh_Hans.po" package/luci-app-autoupdate/po/zh_Hans/autoupdate.po
 
 # MosDNS v5 + geodata
 # Pin to the last known OpenWrt 24.10-compatible package: MosDNS 5.3.3 uses Go 1.22,
@@ -42,20 +45,11 @@ sed -i.bak "s|^PKG_HASH:=.*|PKG_HASH:=${GEOVIEW_HASH}|" package/passwall-package
 rm -f package/passwall-packages/geoview/Makefile.bak
 rm -rf package/passwall-packages/{hysteria,sing-box,v2ray-plugin,xray-core,xray-plugin}
 
-# SSR Plus (helloworld)
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# helloworld 与 PassWall 依赖仓库有大量同名包，只保留 SSR Plus 独有包和 LuCI
-rm -rf package/helloworld/{chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,mosdns,naiveproxy,shadow-tls,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,tuic-client,v2ray-plugin,xray-core,xray-plugin}
-
 # TurboAcc LuCI 界面 (chenmozhijin fork，支持 firewall4/nftables)
 git clone --depth=1 -b luci https://github.com/chenmozhijin/turboacc.git package/turboacc
 # kmod-nft-fullcone (turboacc 依赖，来自 package 分支)
 git clone --depth=1 -b package https://github.com/chenmozhijin/turboacc.git /tmp/turboacc-pkg
 cp -r /tmp/turboacc-pkg/nft-fullcone package/nft-fullcone
 rm -rf /tmp/turboacc-pkg
-
-# vlmcsd KMS Server 二进制包 + LuCI 界面 (openwrt-develop fork，cokebar 原版已归档)
-git clone --depth=1 https://github.com/openwrt-develop/openwrt-vlmcsd.git package/openwrt-vlmcsd
-git clone --depth=1 https://github.com/openwrt-develop/luci-app-vlmcsd.git package/luci-app-vlmcsd
 
 echo ">>> [diy-part1] 完成"
